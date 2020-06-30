69110c473d93fd8d204510b7826cde9f44788382
Faisal Mohamoud <faisal.mohamoud89@gmail.com>
2020-07-01 00:52:41 +0200

    Add local state to AddTransaction component

--- src/components/AddTransaction/AddTransaction.js
+++ src/components/AddTransaction/AddTransaction.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './AddTransaction.module.scss';
 
-const AddTransaction = () => (
-  <div className={styles.addTransaction}>
-    <form>
-      <div className={styles.formControl}>
-        <div className={styles.select}>
-          <select name="type">
-            <option value="income">Income</option>
-            <option value="expense">Expense</option>
-          </select>
+const AddTransaction = () => {
+  const [type, setType] = useState('income');
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(type, name, amount);
+  };
+  return (
+    <div className={styles.addTransaction}>
+      <form onSubmit={onSubmit}>
+        <div className={styles.formControl}>
+          <div className={styles.select}>
+            <select
+              name="type"
+              value={type}
+              onChange={e => setType(e.target.value)}
+            >
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+          </div>
+          <input
+            name="name"
+            type="text"
+            placeholder="Transaction name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <input
+            name="amount"
+            type="number"
+            placeholder="Amount"
+            className={styles.inputAmount}
+            value={amount}
+            onChange={e => setAmount(e.target.value)}
+          />
         </div>
-        <input name="name" type="text" placeholder="Transaction name" />
-        <input
-          name="amount"
-          type="text"
-          placeholder="Amount"
-          className={styles.inputAmount}
-        />
-      </div>
-    </form>
-  </div>
-);
+        <input type="submit" value="Add" hidden />
+      </form>
+    </div>
+  );
+};
 
 export default AddTransaction;
